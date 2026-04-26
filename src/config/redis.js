f8842da2650e95319b4de7be16ad967083a66782
Mohit <mohitswarnkar13@gmail.com>
2026-04-27 02:01:30 +0530

    fix: correct redis debug logging

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,7 @@
 const IORedis = require("ioredis");
 
+console.log("REDIS URL:", process.env.REDIS_URL);
+
 const connection = new IORedis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
 });
